1be1aff27ef10428bd5612bb15731ae2c08f872a
afghifari <alhudaghifari@gmail.com>
2016-11-13 17:50:54 +0700

    inisiasi awal

---
+++ index.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+	  <title>Ilmurah.com</title>
+	  <meta charset="utf-8">
+	  <link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+<body>
+
+	<header>
+		<a href="/BandungNutriCare">
+			<img src="img/logo.png"  alt="Ilmurah.com">
+		</a>
+	</header>
+
+</body>
+</html> 
