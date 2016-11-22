572da6ff07cf1d1f5f3e5f098049cc27f6ff1716
Isham Faizal <i@adimaja.com>
2016-11-22 18:09:47 +0700

    Metalsmith

---
+++ index.js
@@ -0,0 +1,20 @@
+var Metalsmith  = require('metalsmith');
+var layouts     = require('metalsmith-layouts');
+var permalinks  = require('metalsmith-permalinks');
+
+Metalsmith(__dirname)
+  .metadata({
+    title: "My Static Site & Blog",
+    description: "It's about saying »Hello« to the World.",
+    generator: "Metalsmith",
+    url: "http://www.metalsmith.io/"
+  })
+  .source('./src')
+  .destination('./build')
+  .clean(false)
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .build(function(err, files) {
+    if (err) { throw err; }
+  });
